d65959678f7ab09cd1c6a4cfa548c12f9c9adc78
hecki97 <heckinator97@gmx.de>
2014-04-06 22:35:38 +0200

    Update Version 0.6 beta

---
+++ dummy.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>404 Seite nicht gefunden</title>
+	</head>
+	<body>
+		<h1>Achtung! Diese Seite existiert leider noch nicht (Kommt aber hoffentlich bald [Selbstnotiz])</h1>
+	</body>
+</html>
--- index_flash.php
+++ index_flash.php
@@ -3,7 +3,7 @@
   $path = dirname($_SERVER['PHP_SELF']);
 
   if(isset($_REQUEST["fflash"]))
-    header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/index_flash.php');
+    header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/index.php');
 
   $datei_modul2 = "datum_modul2.dat";
   $fp_modul2 = fopen($datei_modul2, "r");
@@ -43,27 +43,26 @@
 
 <body topmargin="0" leftmargin="0" rightmargin="0" bottommargin="0">
 <div class="text">
-  <nobr><span style="text-align:center;"><h2>MLK-Vertretungsplan Online<sup>html</sup></h2></span></nobr>
+  <nobr><span style="text-align:center;"><h2>MLK-Vertretungsplan Online<sup>flash</sup></h2></span></nobr>
   <table style="text-align: left; width:1200; height:800; margin-left: auto; margin-right: auto; margin-bottom: auto;" border="0" cellpadding="0" cellspacing="0">
   <!--<table border=0 bordercolor="#000000" cellpadding=0 cellspacing=0 width="1163" height="658">-->
     <tr>
 			<td colspan="1" rowspan="1" width="585" height="800" style="border-color:#000000;border-width:2px;border-style:solid;">
-        <iframe src="html/modul1.html" name="Vertretungsplan-Modul" scrolling="no" noresize frameborder=0  width="100%" height="100%" style="overflow: hidden;"></iframe> 
+        <iframe src="https://light.dsbcontrol.de/DSBlightWebsite/(S(wns2f3dhy5w21zzxuzomq1ph))/Homepage/PreProgram.aspx?ID=a290d4b8-79f8-4afe-a676-d15b75233151&ID2=d24a5dd7-48c6-4b27-8f79-464a62af12bf" name="Vertretungsplan-Modul" scrolling="no" noresize frameborder=0  width="100%" height="100%" style="overflow: hidden;"></iframe> 
 			</td>
 			<td colspan="1" rowspan="1" width="585" height="800" style="border-color:#000000;border-width:2px;border-style:solid;border-left-style:none;">
-    		<iframe src="html/modul2.html" name="Vertretungsplan-Modul 2" scrolling="no" noresize frameborder=0  width="100%" height="100%" style="overflow: hidden;"></iframe>
+    		<iframe src="https://light.dsbcontrol.de/DSBlightWebsite/(S(wns2f3dhy5w21zzxuzomq1ph))/Homepage/PreProgram.aspx?ID=9de26a53-1a27-40ed-9ee1-82b65a6bcb38&ID2=d24a5dd7-48c6-4b27-8f79-464a62af12bf" name="Vertretungsplan-Modul 2" scrolling="no" noresize frameborder=0  width="100%" height="100%" style="overflow: hidden;"></iframe>
 			</td>
        
 		</tr>
      <caption align="bottom">
         <p>  
           <h3>
-            <span style="text-align:left; margin-right:100px; vertical-align:middle;">Letztes Update (Modul1): <?php echo fgets($fp_modul1); ?></span>
-            <span style="text-align:right; margin-left:100px; vertical-align:middle;">Letztes Update (Modul2): <?php echo fgets($fp_modul2); ?></span>
+            <span style="text-align:center;">Diese Version ist immer aktuell</span>
           </h3>
         </p>
           <form style"text-align:center">
-            <input type="submit" name="fflash" value="Flash Version">
+            <input type="submit" name="fflash" value="HTML Version">
           </form>
       </caption>
 	</table>
