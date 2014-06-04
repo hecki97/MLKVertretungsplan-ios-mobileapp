363d6be39a13c675289def6a5fd04e59d18f6247
hecki97 <heckinator97@gmx.de>
2014-06-04 22:15:29 +0200

    Update 0.9.2 beta

--- mobile/_includeMobile.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include('../_arrayToJSONScript.php');
-	include('../_getVersionScript.php');
-
-	$date_config = "../config/date.config";
-	$mlkvplan_array_modul = DecodeJSONToArray($date_config);
-?>
